d1756f93e2d07f70cdcc3ddd3dbe796fbcc2be07
DEVRAJ SHARMA <70874547+devraj62891@users.noreply.github.com>
2025-06-04 00:07:23 +0530

    modified chatbot

--- .nuxt/dist/server/server.mjs
+++ .nuxt/dist/server/server.mjs
@@ -1,1 +1,1 @@
-export { default } from "file:///home/weloin/Projects/Kamla_Enterprise/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
+export { default } from "file:///D:/Kamla/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
--- components/chat.vue
+++ components/chat.vue
@@ -2,7 +2,7 @@
     <div>
         <div v-if="showChat" @click="showChat = false" class="fixed left-0 right-0 bottom-0 top-0 bg-transparent"></div>
         <!-- Chat Box -->
-        <div v-if="showChat" class="  fixed bottom-24 right-6 bg-green-200 w-74 h-100 p-4 shadow-lg rounded ">
+        <div v-if="showChat" class="  fixed bottom-30 right-6 bg-green-200 w-74 h-100 p-4 shadow-lg rounded ">
             Coming soon...
 
         </div>
@@ -11,8 +11,10 @@
 
         <!-- Chat Button -->
         <UButton @click="chatButtonClick"
-            class="bg-blue-300 fixed bottom-12 right-6 rounded-xl hover:scale-95 hover:bg-blue-500 transition-transform hover:cursor-pointer">
-            Chat
+            class="bg-blue-300 fixed w-15 h-12 shadow-2xl bottom-12 right-6 rounded-xl hover:scale-95 hover:bg-blue-500 transition-transform hover:cursor-pointer">
+            
+            <Icon class="text-3xl items-center"  name="material-symbols:android"/>
+            
         </UButton>
     </div>
 </template>
